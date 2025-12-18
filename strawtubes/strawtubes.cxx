6480c4b6e523c0af1e8780912ba7d06887d34ae3
wcmartylee <wei-chieh.lee@desy.de>
2025-12-18 10:19:26 +0100

    Interim update of CFRP implementation

--- strawtubes/strawtubes.cxx
+++ strawtubes/strawtubes.cxx
@@ -361,6 +361,10 @@ void strawtubes::ConstructGeometry() {
   TGeoVolume* wire = new TGeoVolume("wire", wire_tube, tungsten);
   wire->SetLineColor(6);
 
+  // CFRP plate
+  TGeoBBox* plate = new TGeoBBox("plate", f_aperture_width, f_aperture_height, f_cf_thickness);
+  TGeoVolume* cf_plate = new TGeoVolume("cf_plate", plate, cfrp);
+  
   // Tracking stations
   // statnb = station number; vnb = view number; lnb = layer number; snb = straw
   // number
@@ -437,6 +441,15 @@ void strawtubes::ConstructGeometry() {
           nmview = nmstation + "_y1";
       }
 
+      if(std::fmod(f_design, 3) == 2) {
+	vol->AddNode(cf_plate, statnb * 1e6 + vnb * 1e5 + 7e3, new TGeoTranslation(0, 0, (vnb - 3. / 2.) * f_delta_z_view));
+      } else if(std::fmod(f_design, 3) == 0) {
+	vol->AddNode(cf_plate, statnb * 1e6 + vnb * 1e5 + 6e3,
+		     new TGeoTranslation(0, 0, (vnb - 3. / 2.) * f_delta_z_view - f_delta_z_view / 2. - f_outer_straw_diameter / 2. - f_cf_thickness));
+	vol->AddNode(cf_plate, statnb * 1e6 + vnb * 1e5 + 8e3,
+		     new TGeoTranslation(0, 0, (vnb - 3. / 2.) * f_delta_z_view + f_delta_z_view / 2. + f_outer_straw_diameter / 2. + f_cf_thickness));
+      }
+
       // Adjustments in the stereo views
       // stereo_growth: extension of stereo views beyond aperture
       // stereo_pitch: straw pitch in stereo views
@@ -462,13 +475,15 @@ void strawtubes::ConstructGeometry() {
             f_outer_straw_diameter / 2. + eps / 4);
         TGeoVolume* layerbox = new TGeoVolume(nmlayer, layer, med);
 
-        // The layer box sits in the viewframe.
-        // Hence, z-translate the layer w.r.t. the view
-        vol->AddNode(
-            layerbox, statnb * 1e6 + vnb * 1e5 + lnb * 1e4,
-            new TGeoTranslation(0, -floor_offset / 2.,
-                                (vnb - 3. / 2.) * f_delta_z_view +
-                                    (lnb - 1. / 2.) * f_delta_z_layer));
+        // The layer box sits individually in the station.
+        // Hence, z-translate the layer w.r.t. the station
+	if(std::fmod(f_design, 3) == 2) {
+	  vol->AddNode(layerbox, statnb * 1e6 + vnb * 1e5 + lnb * 1e4,
+		       new TGeoTranslation(0, -floor_offset / 2., (vnb - 3. / 2.) * f_delta_z_view + (lnb - 1. / 2.) * (f_delta_z_layer + f_cf_thickness * 2)));
+	} else {
+	  vol->AddNode(layerbox, statnb * 1e6 + vnb * 1e5 + lnb * 1e4,
+		       new TGeoTranslation(0, -floor_offset / 2., (vnb - 3. / 2.) * f_delta_z_view + (lnb - 1. / 2.) * f_delta_z_layer));
+	}
 
         TGeoRotation r6s;
         TGeoTranslation t6s;
